23452b314b9faa31ca513c6861adf8f1c8df0e50
jguillermol <78916516+jguillermol@users.noreply.github.com>
2021-02-25 09:56:46 -0500

    main+proveedor

--- mainprincipal.cpp
+++ mainprincipal.cpp
@@ -7,9 +7,20 @@
 using namespace std;
 int main() 
 {
-
-	Almacen b;
-	almacenero a;
-	a.actualizar(b);
-	a.visualizar(b);
+	
+	proveedor p[2];
+	Almacen a;
+	int j = 0;
+	do{
+		p[j].ingresar_proveedores();
+		a.agregar_proveedores(&p[j]);
+		j++;	
+	}while(j<2);
+//	a.visualizar_proveedor();
+	
+	string e;
+	system("cls");
+	cout<<endl<<endl<<"ingrese el nombre de la empresa del proveedor: ";cin>>e;
+	a.busqueda_nombre_empresa(e);
+	cout<<endl;
 }
